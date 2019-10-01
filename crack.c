0dbad6c318af9926fc9cc8d545dc8db9d91d7955
mubashir511 <51872713+mubashir511@users.noreply.github.com>
2019-10-01 15:15:18 +0500

    Update crack.c

--- crack.c
+++ crack.c
@@ -9,29 +9,29 @@ int main(int argc, string argv[])
 {
     // hash value of passaward
     string hash_old = "abBZmQBTazHcs";
-    // Finding correct key 
+    // Finding correct key
     if (argv[1] == NULL)
     {
-        printf("Usage: ./caesar key\n");
+        printf("Usage: ./crack password");
         return 1;
     }
-    // Getting input string 
-    string s = argv[1]; 
-    // Finding the length of the string 
+    // Getting input string
+    string s = argv[1];
+    // Finding the length of the string
     int c = strlen(s);
     // If only one argument is present
     if ((argc == 2) && (c <= 5))
     {
-        // Checking for numeric digits 
+        // Checking for numeric digits
         for (int i = 0; i < c; i++)
         {
             if (isdigit(s[i]) == 0)
             {
                 if (((s[i] >= 'a') && (s[i] <= 'z')) || ((s[i] >= 'A') && (s[i] <= 'Z')))
                 {
-                    s[i] = s[i] + 0;   
+                    s[i] = s[i] + 0;
                 }
-                else 
+                else
                 {
                     printf("invalid keyword\n");
                     return 1;
@@ -39,16 +39,16 @@ int main(int argc, string argv[])
             }
             else
             {
-                printf("Usage: ./caesar key\n");
+                printf("Usage: ./crack password");
                 return 1;
             }
-        } 
+        }
     }
     else
     {
-        printf("Usage: ./caesar key\n");
+        printf("Usage: ./crack password");
         return 1;
-    } 
+    }
     // Computing hash value of the give valu
     string hash_new = crypt(s, "ab");
     // Comparing both hashes
@@ -57,11 +57,11 @@ int main(int argc, string argv[])
         printf("passward: %s\n", s);
         return 0;
     }
-    else 
+    else
     {
         printf("your passward is incorrect\n");
-        return 0;    
+        return 0;
     }
-    
+
 }
 
