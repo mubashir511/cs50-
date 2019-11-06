0d11f2c7409aadfac75d5896c0e3c7d0a735b580
mubashir511 <ubuntu@ide.ide.3dcde01e-f68c-4a1e-b7af-99ac3690bef8.svc.cluster.local>
2019-11-06 18:01:43 +0000

    deleted all unecessary filese from test branch

--- pset6/hello.py
+++
@@ -1,6 +0,0 @@
-# Importing get_string function from cs50 library
-from cs50 import get_string
-# Getting name from the user
-name = get_string("What is your name? ")
-# Print the given name
-print(f"hello, {name}")
